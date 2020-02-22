11bf9581c28f8d31ba9dd0a4e4769ac8c5a3a875
WoundR <61352731+WoundR@users.noreply.github.com>
2020-02-22 18:33:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,151 @@
+function show(id)
+{
+	document.getElementById('methodCaesar').style.display = 'block'
+	document.getElementById('esar').style.display = 'none'
+
+}
+function sho(id)
+{
+	document.getElementById('esar').style.display = 'block'
+	document.getElementById('methodCaesar').style.display = 'none'
+
+}
+function barb(id)
+{
+	document.getElementById('barbarosa').style.display = 'block'
+	document.getElementById('barbarosaVID1').style.display = 'block'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function t(id)
+{
+	document.getElementById('tif').style.display = 'block'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function y(id)
+{
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'block'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function k(id)
+{
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'block'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function isk(id)
+{
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'block'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function cit(id)
+{
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'block'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function kut(id)
+{
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'block'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function bag(id)
+{
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'block'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function vis(id)
+{
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'block'
+	document.getElementById('berl').style.display = 'none'
+}
+function b(id)
+{
+	document.getElementById('berl').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'block'
+}
